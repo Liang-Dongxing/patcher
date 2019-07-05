aa8a91f153f4672f4836bb98c6340f3d5dc8187e
Liang-Dongxing <liangyongseven@outlook.com>
2019-07-05 11:48:48 +0800

    根据 192 Win10无边框 UI 优化布局

--- src/com/ldx/PatcherDialog.java
+++ src/com/ldx/PatcherDialog.java
@@ -25,6 +25,9 @@ import sun.awt.SunToolkit;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.Arrays;
@@ -85,6 +88,7 @@ public class PatcherDialog extends JDialog {
     private void initDialog(AnActionEvent event) {
         super.setSize(600, 400);
         setLocation(event);
+        super.setUndecorated(true);
         super.setVisible(true);
         super.requestFocus();
     }
@@ -114,6 +118,8 @@ public class PatcherDialog extends JDialog {
             });
 
             buttonCancel.addActionListener(e -> dispose());
+
+            contentPane.registerKeyboardAction(e -> dispose(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
             initDialog(event);
         } else {
