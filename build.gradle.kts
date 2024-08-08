511018289add8471d2224e5f14486a81b23efe2f
LiangYong <liangdongxing.cn@outlook.com>
2024-08-08 10:57:42 +0800

    feat(升级): 升级支持2024.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.bit"
-version = "2024.2"
+version = "2024.3"
 
 repositories {
     mavenCentral()
@@ -13,7 +13,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2024.1")
+    version.set("2024.2")
     type.set("IU") // Target IDE Platform
 
     plugins.set(listOf("com.intellij.java"))
